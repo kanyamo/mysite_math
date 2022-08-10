a13591d5097b05d4f835353ce6b84d63082c6be0
kanyamo <kanajir@yahoo.co.jp>
2022-08-10 15:25:25 +0900

    admin登録

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MyUser, Article, Content, Headline, Paragraph, Image
 
 # Register your models here.
+admin.site.register(MyUser)
+admin.site.register(Article)
+admin.site.register(Content)
+admin.site.register(Headline)
+admin.site.register(Paragraph)
+admin.site.register(Image)
