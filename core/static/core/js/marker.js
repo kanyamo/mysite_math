49d3c0c8d5d9e9fb0cdcaef4e16d79264c120bc4
kanyamo <kanajir@yahoo.co.jp>
2022-09-30 01:25:11 +0900

    スクロールしなくても最初に一回マーカーのアクティベート判定を行う

--- core/static/core/js/marker.js
+++ core/static/core/js/marker.js
@@ -3,13 +3,20 @@ function isHeightViewable(element){
     return top >= 0 && bottom <= window.innerHeight;
 }
 
+function activateViewableElement(element){
+    if (isHeightViewable(element) && !element.classList.contains('active')) {
+        element.classList.add('active');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     marks = document.querySelectorAll('mark');
+    marks.forEach(mark => {
+        activateViewableElement(mark);
+    });
     window.addEventListener('scroll', () => {
         marks.forEach(mark => {
-            if (isHeightViewable(mark) && !mark.classList.contains('active')) {
-                mark.classList.add('active');
-            }
+            activateViewableElement(mark);
         });
     });
 });
