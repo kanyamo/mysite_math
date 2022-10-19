1b456b89e77ffb76d2ff4203cf1ddd8bb3ffc403
kanyamo <kanajir@yahoo.co.jp>
2022-10-20 02:54:20 +0900

    preload.jsのタイムアウト時間を変更

--- core/static/core/js/preload.js
+++ core/static/core/js/preload.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.setTimeout(() => {
         const body = document.getElementById('body');
         body.classList.remove('preload');
-    }, 100);
+    }, 300);
 });
